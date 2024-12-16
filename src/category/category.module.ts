4dda0f4f0703bf9c6d32e6dc9457729e67ee787c
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-16 23:08:19 +0100

    refactor: modifications dans le module des catégories

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from './schema/cateogry.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: Category.name, schema: CategorySchema}])
+  ],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
