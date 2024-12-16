84b61e7daf86660687bd9f67a49441764f466cd0
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-16 13:59:49 +0100

    fix: update user schema for database alignment

--- src/users/schema/user.schema.ts
+++ src/users/schema/user.schema.ts
@@ -17,7 +17,7 @@ export class User {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ enum: ['user', 'organisateur'], default: 'user' })
+  @Prop({ enum: ['client', 'admin' ], default: 'client' })
   role: string;
 }
 
