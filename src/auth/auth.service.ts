116f823315662b3fea1a28e092abd145353740a5
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-16 15:04:43 +0100

    fix: improve service methods in auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateAuthDto, Role } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from 'src/users/schema/user.schema';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
@@ -12,29 +11,51 @@ export class AuthService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
     private readonly jwtService: JwtService,
-
-  ) { }
-
+  ) {}
 
   async register(userDto: CreateAuthDto): Promise<any> {
     const { firstName, lastName, email, password, role } = userDto;
 
     const existingUser = await this.userModel.findOne({ email });
     if (existingUser) {
-      throw new Error('User with this email already exists');
+      throw new HttpException(
+        'User with this email already exists',
+        HttpStatus.BAD_REQUEST,
+      );
     }
-    
+
     const hashedPassword = await bcrypt.hash(password, 10);
-    const userRole = role || Role.Client
-    const CreateUser = await this.userModel.create({
+    const userRole = role || Role.Client;
+
+    const newUser = await this.userModel.create({
       firstName,
       lastName,
       email,
-      password:hashedPassword ,
+      password: hashedPassword,
       role: userRole,
     });
 
-    return  CreateUser.save();
+    return newUser.save();
+  }
 
+  async login(email: string, password: string): Promise<{ token: string }> {
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      throw new HttpException(
+        'User with this email does not exist',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+    if (!passwordValid) {
+      throw new HttpException('Invalid password', HttpStatus.UNAUTHORIZED);
+    }
+
+    const payload = { email: user.email, id: user._id, role: user.role };
+    const token = this.jwtService.sign(payload);
+
+    return { token };
   }
 }
