b37cb36d286ce79721e38f260a8b7a300af3df38
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-16 19:48:25 +0100

    fix: adjust authentication controller logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
+import { Controller, Post, Body, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('auth')
 export class AuthController {
@@ -9,19 +12,47 @@ export class AuthController {
 
 
 
-  @Post('/register')
-  async register(@Body() userDTO: CreateAuthDto){
-    try{
-      const userRregitred = await this.authService.register(userDTO)
-      return {
-        message: 'User been registered successfully',
-        user: userRregitred,
-      };
-    } catch (error) {
-      throw error;
+  
+    @Post('/register')
+    @UseInterceptors(
+      FileInterceptor('avatar', {
+        storage: diskStorage({
+          destination: './uploads', 
+          filename: (req, file, callback) => {
+            const uniqueSuffix = Date.now() ;
+            const ext = extname(file.originalname);
+            callback(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+          },
+        }),
+        fileFilter: (req, file, callback) => {
+          if (!file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+            callback(new Error('Only image files are allowed!'), false);
+          }
+          callback(null, true);
+        },
+      }),
+    )
+    async register(
+      @Body() userDTO: CreateAuthDto, 
+      @UploadedFile() file: Express.Multer.File, 
+    ) {
+      try {
+        if (!file) {
+          throw new Error('File upload failed.');
+        }
+        
+        const userRegistered = await this.authService.register(userDTO, file.path);
+        
+        return {
+          message: 'User has been registered successfully',
+          user: userRegistered,
+        };
+      } catch (error) {
+        throw error;
+      }
     }
-    
-  }
+
+  
 
 
   @Post('/login')
@@ -33,5 +64,21 @@ export class AuthController {
       return await this.authService.login(email, password);
     } catch (error) {
       throw error;
-    }  }
+    }
+   }
+
+   @Post('forget-password')
+  async forgetPassword(@Body('email') email: string) {
+    return await this.authService.forgetPassword(email);
+  }
+
+  @Post('reset-password')
+  async resetPassword(
+    @Body('token') token: string,
+    @Body('newPassword') newPassword: string,
+  ) {
+    return await this.authService.resetPassword(token, newPassword);
+  }
+
+  
 }
