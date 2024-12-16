2fef410eba41df22607d2d1fb3edba25fe3c203d
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-16 15:04:29 +0100

    fix: update authentication controller with new logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,4 +7,31 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+
+
+  @Post('/register')
+  async register(@Body() userDTO: CreateAuthDto){
+    try{
+      const userRregitred = await this.authService.register(userDTO)
+      return {
+        message: 'User been registered successfully',
+        user: userRregitred,
+      };
+    } catch (error) {
+      throw error;
+    }
+    
+  }
+
+
+  @Post('/login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    try {
+      return await this.authService.login(email, password);
+    } catch (error) {
+      throw error;
+    }  }
 }
