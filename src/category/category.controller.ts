36f5f441873a7e7822f4ae60e4bcaa55ef64ca54
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-16 23:08:13 +0100

    fix: corrections dans le contrôleur des catégories

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -7,28 +7,10 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
-  @Post()
+  @Post('/create_Category')
   create(@Body() createCategoryDto: CreateCategoryDto) {
+    
     return this.categoryService.create(createCategoryDto);
   }
 
-  @Get()
-  findAll() {
-    return this.categoryService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoryService.update(+id, updateCategoryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
-  }
 }
