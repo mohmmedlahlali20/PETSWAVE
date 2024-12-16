3bd22d0944c73ff1bc2288beb46b2bb599ddbaa4
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-16 23:36:28 +0100

    refactor: mise à jour du module des animaux

--- src/pets/pets.module.ts
+++ src/pets/pets.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PetsService } from './pets.service';
 import { PetsController } from './pets.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pets, PetsSchema } from './schema/pets.schema';
 
 @Module({
+  imports : [
+        MongooseModule.forFeature([{name: Pets.name, schema: PetsSchema}])
+    
+  ],
   controllers: [PetsController],
   providers: [PetsService],
 })
