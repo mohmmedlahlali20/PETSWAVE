b49a4177de8f5734fd725a87fcdcd67fb88ba7da
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-16 23:36:18 +0100

    feat: mise à jour du DTO pour la création des animaux

--- src/pets/dto/create-pet.dto.ts
+++ src/pets/dto/create-pet.dto.ts
@@ -1,1 +1,28 @@
-export class CreatePetDto {}
+import { IsString, IsNotEmpty, IsNumber, IsEnum, IsArray, ArrayNotEmpty, IsMongoId } from 'class-validator';
+
+export class CreatePetDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEnum(['Male', 'Female'])
+  gender: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
+
+  @IsNotEmpty()
+  @IsMongoId()
+  category: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  images: string[];
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+}
