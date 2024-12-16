025c75518586b4768087f54a2ed7169cecbc20ce
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-16 13:59:42 +0100

    fix: update auth.service for improved service methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import { CreateAuthDto, Role } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from 'src/users/schema/user.schema';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+    private readonly jwtService: JwtService,
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  ) { }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+  async register(userDto: CreateAuthDto): Promise<any> {
+    const { firstName, lastName, email, password, role } = userDto;
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    const existingUser = await this.userModel.findOne({ email });
+    if (existingUser) {
+      throw new Error('User with this email already exists');
+    }
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userRole = role || Role.Client
+    const CreateUser = await this.userModel.create({
+      firstName,
+      lastName,
+      email,
+      password:hashedPassword ,
+      role: userRole,
+    });
+
+    return  CreateUser.save();
+
   }
 }
