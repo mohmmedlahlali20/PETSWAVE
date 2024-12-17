0657599fb7e21d33de07ce1eb41ccc059e97174a
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-17 11:33:03 +0100

    fix: mise à jour du contrôleur des animaux

--- src/pets/pets.controller.ts
+++ src/pets/pets.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, Body, UseInterceptors, UploadedFiles, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors, UploadedFiles, Get, Param, Delete, Patch } from '@nestjs/common';
 import { PetsService } from './pets.service';
-import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { FileFieldsInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { CreatePetDto } from './dto/create-pet.dto';
+import { UpdatePetDto } from './dto/update-pet.dto';
 
 @Controller('pets')
 export class PetsController {
@@ -50,6 +51,42 @@ export class PetsController {
   async getPetsByCategoryId(@Param('categoryId')  categoryId:string ){
     return this.petsService.getPetsByCategoryID(categoryId)
   }
+
+  @Delete('/delete/:petsId')
+  async deletePets(@Param('petsId') petsId: string){
+    return this.petsService.removePets(petsId)
+  }
+
+  @Patch('/update/:petsId')
+@UseInterceptors(FilesInterceptor('images'))
+async updatePets(
+  @Param('petsId') petsId: string,
+  @Body() updateDTO: UpdatePetDto,
+  @UploadedFiles() files: Express.Multer.File[],
+): Promise<any> {
+  try {
+    const imagePaths = files?.map(file => file.path) || [];
+
+    const existingPet = await this.petsService.getPetsById(petsId);
+
+    if (!existingPet) {
+      return { message: 'Pet not found' };
+    }
+
+    const updatedImages = [...existingPet.images, ...imagePaths];
+
+    const updatedPet = await this.petsService.update(petsId, { ...updateDTO, images: updatedImages });
+
+    return {
+      message: 'Pet updated successfully',
+      updatedPet,
+    };
+  } catch (error) {
+    console.error('Error while updating pet:', error);
+    throw new Error('Failed to update pet');
+  }
+}
+
 
   
 
