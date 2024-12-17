6839875a17cddef583002856f31a2582f04cd01e
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-17 11:33:08 +0100

    fix: correction et amélioration du module des animaux

--- src/pets/pets.module.ts
+++ src/pets/pets.module.ts
@@ -3,11 +3,14 @@ import { PetsService } from './pets.service';
 import { PetsController } from './pets.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Pets, PetsSchema } from './schema/pets.schema';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports : [
-        MongooseModule.forFeature([{name: Pets.name, schema: PetsSchema}])
-    
+        MongooseModule.forFeature([{name: Pets.name, schema: PetsSchema}]),
+        MulterModule.register({
+          dest: './petsIMG',
+        }),
   ],
   controllers: [PetsController],
   providers: [PetsService],
