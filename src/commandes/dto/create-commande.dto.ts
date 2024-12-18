fe8b6774ca24cc9d10ca7ed0584018c0caaa718a
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-18 09:42:01 +0100

    feat: ajout du DTO pour la création de commandes

--- src/commandes/dto/create-commande.dto.ts
+++ src/commandes/dto/create-commande.dto.ts
@@ -1,1 +1,37 @@
-export class CreateCommandeDto {}
+import { IsMongoId, IsNotEmpty, IsNumber } from "class-validator";
+
+export interface status {
+    Pending: 'pending',
+    InProgress: 'in progress',
+    Completed: 'completed',
+    Cancelled: 'cancelled'
+}
+
+export class CreateCommandeDto {
+    @IsMongoId()
+    @IsNotEmpty()
+    petsId: string
+
+
+    @IsMongoId()
+    @IsNotEmpty()
+    userId: string
+
+
+    @IsNotEmpty()
+    status: status
+
+
+    @IsNotEmpty()
+    orderDate: Date;
+
+
+
+    @IsNotEmpty()
+    @IsNumber()
+    totalAmount: number
+
+
+
+
+}
