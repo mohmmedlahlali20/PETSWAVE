b1334fc794eec3ec67487279473e8bab4354d345
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-18 09:41:55 +0100

    fix: mise à jour du module commandes pour inclure les nouveaux services

--- src/commandes/commandes.module.ts
+++ src/commandes/commandes.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CommandesService } from './commandes.service';
 import { CommandesController } from './commandes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Commande, CommandeSchema } from './schema/command.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: Commande.name, schema:CommandeSchema}])
+  ],
   controllers: [CommandesController],
   providers: [CommandesService],
 })
