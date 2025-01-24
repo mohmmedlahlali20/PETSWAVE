2185ed847b54a467597e15fa9e7083a26738a1fa
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-24 15:14:46 +0100

    refactor: updated application bootstrap logic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  console.log(`servuer is runing in port : http://localhost:${process.env.PORT}`)
   await app.listen(process.env.PORT);
 }
 bootstrap();
