1797f00a110e6c3c1b347d085f1a1d511336ddb3
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-25 19:36:09 +0100

    chore(main): adjusted main file configuration for app initialization

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   console.log(`this projet runing in this port : http://localhost:${process.env.PORT}`)
   await app.listen(process.env.PORT);
 }
