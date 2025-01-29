ead7a8941c0cad7acc586394a086db81ef4c0c0d
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-29 19:03:10 +0100

    fix(category): update category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -5,12 +5,17 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('category')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+  constructor(private readonly categoryService: CategoryService) { }
 
   @Post('/create_Category')
   create(@Body() createCategoryDto: CreateCategoryDto) {
-    
+
     return this.categoryService.create(createCategoryDto);
+  }
+
+  @Get('/GetAll')
+  async getAllCategory() {
+    return this.categoryService.findAll()
   }
 
 }
