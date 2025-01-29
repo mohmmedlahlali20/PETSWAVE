1bebea6e36407cd1efada2696569f814c421a536
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-29 19:59:08 +0100

    fix(users): update users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './schema/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly UsersModule: Model<UserDocument>){}
+
+
+
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    try {
+      const users = await this.UsersModule.find().exec();
+      return users;
+    } catch (err) {
+      console.error('Error while fetching all users:', err);
+      throw new Error('Failed to get all users');
+    }
   }
 
   findOne(id: number) {
