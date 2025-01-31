eb533465bb81a09246e90b6abf8157f60798dfcd
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-31 20:39:18 +0100

    fix(commandes): update commandes module

--- src/commandes/commandes.module.ts
+++ src/commandes/commandes.module.ts
@@ -3,10 +3,16 @@ import { CommandesService } from './commandes.service';
 import { CommandesController } from './commandes.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Commande, CommandeSchema } from './schema/command.schema';
+import { PetsModule } from 'src/pets/pets.module';
+import { Pets, PetsSchema } from 'src/pets/schema/pets.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{name: Commande.name, schema:CommandeSchema}])
+    MongooseModule.forFeature([
+      {name: Commande.name, schema:CommandeSchema},
+      { name: Pets.name, schema: PetsSchema },
+    ]),
+    PetsModule,
   ],
   controllers: [CommandesController],
   providers: [CommandesService],
