ae70f8a5e1d89de2e2f86ca6a7eece2c334c3027
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-31 20:39:17 +0100

    fix(commandes): update commandes controller

--- src/commandes/commandes.controller.ts
+++ src/commandes/commandes.controller.ts
@@ -2,14 +2,18 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CommandesService } from './commandes.service';
 import { CreateCommandeDto } from './dto/create-commande.dto';
 import { UpdateCommandeDto } from './dto/update-commande.dto';
+import { PetsService } from 'src/pets/pets.service';
 
 @Controller('commandes')
 export class CommandesController {
-  constructor(private readonly commandesService: CommandesService) {}
+  constructor(
+    private readonly commandesService: CommandesService,
+    private readonly PetsService: PetsService,
+  ) {}
 
-  @Post()
+  @Post("/Passe_commandes")
   create(@Body() createCommandeDto: CreateCommandeDto) {
-    
+    return this.commandesService.create(createCommandeDto);
   }
 
  
