64dc86c9363df65edabcb3665c4914502e2942b1
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-03-16 15:43:19 +0000

    feat(category): enhance category controller functionality

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -6,22 +14,22 @@ import { Category } from './schema/cateogry.schema';
 
 @Controller('category')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) { }
+  constructor(private readonly categoryService: CategoryService) {}
 
   @Post('/create_Category')
   create(@Body() createCategoryDto: CreateCategoryDto) {
-
     return this.categoryService.create(createCategoryDto);
   }
 
   @Get('/GetAll')
   async getAllCategory() {
-    return this.categoryService.findAll()
+    return this.categoryService.findAll();
   }
 
   @Delete(':categoryId')
   async removeCategory(@Param('categoryId') categoryId: string) {
-    const deletedCategory = await this.categoryService.removeCategory(categoryId);
+    const deletedCategory =
+      await this.categoryService.removeCategory(categoryId);
     if (deletedCategory) {
       return { message: 'Category deleted successfully', deletedCategory };
     } else {
@@ -36,5 +44,4 @@ export class CategoryController {
   ): Promise<Category | null> {
     return this.categoryService.updateCategory(updateCategoryDto, categoryId);
   }
-
 }
