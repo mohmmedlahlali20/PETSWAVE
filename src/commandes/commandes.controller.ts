630aa378b210df7b66851583e81204ec8c6709a0
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-03-16 15:43:22 +0000

    feat(commandes): add new endpoints to commandes controller

--- src/commandes/commandes.controller.ts
+++ src/commandes/commandes.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CommandesService } from './commandes.service';
 import { CreateCommandeDto } from './dto/create-commande.dto';
-import { UpdateCommandeDto } from './dto/update-commande.dto';
-import { PetsService } from 'src/pets/pets.service';
 
 @Controller('commandes')
 export class CommandesController {
   constructor(private readonly commandesService: CommandesService) {}
 
-  @Post("/Passe_commandes")
+  @Post('/Passe_commandes')
   create(@Body() createCommandeDto: CreateCommandeDto) {
     return this.commandesService.create(createCommandeDto);
   }
@@ -18,24 +16,23 @@ export class CommandesController {
     const allCommandes = await this.commandesService.getAllCommandes();
     return {
       message: 'All Commandes',
-      allCommandes
-    }
+      allCommandes,
+    };
   }
 
-  @Get('/GetCommandeByUserId/:userId')  
+  @Get('/GetCommandeByUserId/:userId')
   async getCommandeByUserId(@Param('userId') userId: string) {
-    const userCommandes = await this.commandesService.getCommandeByUserId(userId);
+    const userCommandes =
+      await this.commandesService.getCommandeByUserId(userId);
     if (!userCommandes) {
       return {
         message: 'No Commandes found for this user',
-        userCommandes: []
-      }
+        userCommandes: [],
+      };
     }
     return {
       message: 'Commandes found for this user',
-      userCommandes
-    }
+      userCommandes,
+    };
   }
-
- 
 }
