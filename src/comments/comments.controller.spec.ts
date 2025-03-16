9e79c791ccd06567a35c223598c6ce46a4c8bec6
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-03-16 21:37:28 +0000

    test(comments): update test cases for comments controller

--- src/comments/comments.controller.spec.ts
+++ src/comments/comments.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Comments } from './schemas/comment.schema';
 
 describe('CommentsController', () => {
   let controller: CommentsController;
@@ -8,7 +10,13 @@ describe('CommentsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentsController],
-      providers: [CommentsService],
+      providers: [
+        CommentsService,
+        {
+          provide: getModelToken(Comments.name),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<CommentsController>(CommentsController);
