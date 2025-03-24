0b582d7d280800e50d6da1b8d05362087943445f
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-03-24 10:41:36 +0000

    fix(guards): enhance authentication guard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,4 +1,9 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 import * as process from 'node:process';
 
@@ -7,29 +12,19 @@ export class JwtAuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const authHeader = request.headers['authorization'];
-
     if (!authHeader) {
-      throw new UnauthorizedException('No token provided');
+      return null
     }
-
     const token = authHeader.split(' ')[1];
     if (!token) {
       throw new UnauthorizedException('Malformed authorization header');
     }
-
     try {
       const secret = process.env.JWT_SECRET;
       if (!secret) {
         throw new Error('JWT_SECRET is not defined in environment variables');
       }
-
-      console.log('Using Secret:', secret);
-      console.log('Token:', token);
-
-      const decoded = jwt.verify(token, secret);
-      console.log('Decoded Token:', decoded);
-
-      request.user = decoded;
+      request.user = jwt.verify(token, secret);
       return true;
     } catch (err) {
       console.error('JWT Verification Error:', err.message);
